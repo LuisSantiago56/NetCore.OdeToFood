942998526f6f780d91b9a6326885c01dc74d1abf
Luis Santiago <luis.satiago56@upr.edu>
2020-07-20 16:03:42 -0600

    Developed funtionalities like Add and Update with the Add and Edit Views. Created the DbContext and Connection String. Need to work on the Migrations.

--- OdeToFood.Data/InMemoryRestaurantData.cs
+++ OdeToFood.Data/InMemoryRestaurantData.cs
@@ -17,6 +17,18 @@ namespace OdeToFood.Data
             };
         }
 
+        public Restaurant Add(Restaurant newRestaurant)
+        {
+            restaurants.Add(newRestaurant);
+            newRestaurant.Id = restaurants.Max(r => r.Id) + 1;
+            return newRestaurant;
+        }
+
+        public int Commit()
+        {
+            return 0;
+        }
+
         public Restaurant GetById(int id)
         {
             return restaurants.SingleOrDefault(r => r.Id == id);
@@ -27,6 +39,18 @@ namespace OdeToFood.Data
                    where string.IsNullOrEmpty(name) || r.Name.StartsWith(name)
                    orderby r.Name
                    select r;
+        }
+
+        public Restaurant Update(Restaurant updatedRestaurant)
+        {
+            var restaurant = restaurants.SingleOrDefault(r => r.Id == updatedRestaurant.Id);
+            if(restaurant != null)
+            {
+                restaurant.Name = updatedRestaurant.Name;
+                restaurant.Location = updatedRestaurant.Location;
+                restaurant.Cuisine = updatedRestaurant.Cuisine;
+            }
+            return restaurant;
         }
     }
 }
--- OdeToFood.Web/Pages/Restaurants/Detail.cshtml.cs
+++ OdeToFood.Web/Pages/Restaurants/Detail.cshtml.cs
@@ -12,15 +12,22 @@ namespace OdeToFood.Web.Pages.Restaurants
     public class DetailModel : PageModel
     {
         private readonly IRestaurantData restaurantData;
+        [TempData]
+        public string Message { get; set; }
 
         public Restaurant Restaurant { get; set; }
         public DetailModel(IRestaurantData restaurantData)
         {
             this.restaurantData = restaurantData;
         }
-        public void OnGet(int restaurantId)
+        public IActionResult OnGet(int restaurantId)
         {
             Restaurant = restaurantData.GetById(restaurantId);
+            if (Restaurant == null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+            return Page();
         }
     }
 }
